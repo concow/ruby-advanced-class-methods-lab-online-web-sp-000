269ea39bc8cad94226225b9dfbb4315cff335187
Connor Cowan <ccowan003@comcast.net>
2019-06-26 20:27:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,14 @@ class Song
   end
 
   def self.create_from_filename
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
 
+  song = self.create
+  song.name = song_name
+  song.artist_name = artist_name
+  return song
   end
 
   def self.destroy_all
